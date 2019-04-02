4baf1a6afa6aa4fe41928dce06aa500a7a20cfa3
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-02 13:42:23 +0900

    19_04_02_1342

---
+++ mathOps.py
@@ -0,0 +1,30 @@
+print('Prints out math operators')
+
+print('Exponent 2**3 = 8')
+a = 2**3
+print(a)
+
+print('Modulus 22%8 = 6')
+b = 22%8
+print(b)
+
+print('Int division 22//8 = 2')
+c = 22//8
+print(c)
+
+print('Division 22/8 = 2.75')
+d = 22/8
+print(d)
+
+print('Multiplication 3*5 = 15')
+e = 3*5
+print(e)
+
+print('Sub 5-2 = 3')
+f = 5-2
+print(f)
+
+print('Add 2+2 = 4')
+g = 2+2
+print(g)
+
