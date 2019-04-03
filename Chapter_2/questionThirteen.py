b29d35ed65a87482211c7bc102a45c4d09018bb5
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-03 13:58:13 +0900

    1358

---
+++ Chapter_2/questionThirteen.py
@@ -0,0 +1,13 @@
+# 1-10 using a for loop
+print('For loop.')
+i = 0
+for i in range (10):
+    print(str(i+1))
+
+print()
+# 1-10 using a while loop
+print('While loop.')
+j = 0
+while j < 10:
+    print(str(j+1))
+    j = j + 1
