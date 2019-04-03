7cdb4d484c320d52e3761971b5849798b438e2d4
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-03 14:31:49 +0900

    1431

---
+++ Chapter_2/extraCredits.py
@@ -0,0 +1,16 @@
+# Using round()
+print('This function is to use round()')
+print('Type in a number with decimals')
+number = float(input())
+print('How many places do you want to round to?')
+rPoint = int(input())
+
+print(round(number,rPoint))
+
+print()
+# Using abs()
+print('This function is to use abs()')
+print('What number do you want to find the absolute of?')
+absolute = float(input())
+
+print(abs(absolute))
