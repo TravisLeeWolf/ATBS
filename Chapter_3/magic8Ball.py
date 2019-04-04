d8f7070427affae133b717796d7fdd760f357f9d
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-04 10:12:53 +0900

    1012

---
+++ Chapter_3/magic8Ball.py
@@ -0,0 +1,10 @@
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidely so'
+    elif answerNumber == 3:
+        return 'Yes'
+    
