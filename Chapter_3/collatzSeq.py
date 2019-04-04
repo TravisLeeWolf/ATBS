59f96d70cc34f8018079163ca031f038d9060417
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-04 15:33:17 +0900

    1533

---
+++ Chapter_3/collatzSeq.py
@@ -0,0 +1,26 @@
+# Define the function
+def collatz(number):
+    # Add a variable to return
+    answer = 0
+    # Using mod 2 to find if it is odd or even we can split the cases
+    if number % 2 == 0:
+        answer = number // 2
+    else:
+        answer = 3 * number + 1
+    return answer
+
+# Get user input
+print('Let us use the collatz function.')
+print('Type a number that is odd or even.')
+# Here it has to run collatz() until the answer is 1
+try:
+    userNum = int(input())
+    check = collatz(userNum)
+    while check != 1:
+        collatz(userNum)
+    else:
+        print(check)
+except ValueError:
+    print('Please type in a positive integer value.')
+
+# Call the function
