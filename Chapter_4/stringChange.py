ec4a51213b9f3df9a23cd9d39bcef94b6d97f94b
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-05 11:23:57 +0900

    1123

---
+++ Chapter_4/stringChange.py
@@ -0,0 +1,10 @@
+# Running through a string like a list
+name = 'Zophie'
+for i in name:
+    print('**' + i + '**')
+
+# Editing a sting
+name = 'Zophie a cat'
+newName = name[0:7] + 'the' + name[8:12]
+print(name)
+print(newName)
