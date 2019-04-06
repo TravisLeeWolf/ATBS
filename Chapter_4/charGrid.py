cdcf0aaec91b3ba246a89ab0ac865cb161a05a29
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-06 20:22:02 +0900

    2021

--- Chapter_4/charGrid.py
+++ Chapter_4/charGrid.py
@@ -1,6 +1,8 @@
 def showGrid(dispGrid):
-    for i in range(len(dispGrid)):
-        for j in range(len(dispGrid[i])):
+    h = 0
+    for i in range(len(dispGrid[h])):
+        h = h + 1
+        for j in range(len(dispGrid)):
             print(dispGrid[j][i], end='')
         print()
 
