00c9f5ac1e65617bde46dd9d59fc5cc21d4778ef
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-06 19:29:47 +0900

    1929

--- Chapter_4/charGrid.py
+++ Chapter_4/charGrid.py
@@ -1,1 +1,23 @@
-# Complete this today
+def showGrid(dispGrid):
+    i =0
+    if i < (len(dispGrid)):
+        j = 0
+        while j < (len(dispGrid[i])):
+            print(dispGrid[i][j], end='')
+            j =+ 1
+        i =+ 1
+    else:
+        print()
+
+
+heart = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+showGrid(heart)
