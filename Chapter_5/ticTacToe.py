848a2ef608824bbac5afe92d59728bd2093814ae
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-07 11:54:59 +0900

    1154

---
+++ Chapter_5/ticTacToe.py
@@ -0,0 +1,28 @@
+# A standard tic-tac-toe board
+# ' ' indicate an empty block
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+# Function to print the board
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+# Allow user to place 'peices' on the board
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+# Call the funtion
+printBoard(theBoard)
