292b948fa64de021f86531160b86c429ee89239e
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-08 21:18:22 +0900

    2118

---
+++ Chapter_6/catNapping.py
@@ -0,0 +1,13 @@
+# Using escape characters
+print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping.\n\nSincerely,\nBob')
+
+# Using triple quotes
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+"""You can use multi-line quotes
+to further explain parts of you code"""
---
+++ Chapter_6/upLow.py
@@ -0,0 +1,7 @@
+# Using lower to make a case insensitive evaluation
+print('How are you?')
+feeling = input()
+if feeling.lower() == 'great':
+    print('I feel great too!')
+else:
+    print('I hope the rest of your day is good.')
