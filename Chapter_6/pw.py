ddb42789d49d9318d4a3079487fe2861b199a688
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-08 22:33:34 +0900

    2233

---
+++ Chapter_6/pw.py
@@ -0,0 +1,17 @@
+#! python3
+# pw.py - An insecure password locker program.
+
+# Password dictionary to store passwords
+PASSWORDS = {'email' : 'th1s1smyp455w0rd',
+            'blog' : 'th1st001sm1n3',
+            'luggage' : '12345'}
+
+# Handle command line arguments
+import sys
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]   # First command line arg is the account name
+
+# Copy the right password
