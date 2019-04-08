f5ee53bdf2297f0b9451eb0ed87453618bfca827
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-08 22:43:55 +0900

    2243

--- Chapter_6/pw.py
+++ Chapter_6/pw.py
@@ -7,7 +7,7 @@ PASSWORDS = {'email' : 'th1s1smyp455w0rd',
             'luggage' : '12345'}
 
 # Handle command line arguments
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
@@ -15,3 +15,10 @@ if len(sys.argv) < 2:
 account = sys.argv[1]   # First command line arg is the account name
 
 # Copy the right password
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named' + account)
+
+# pw.bat created in C:\Windows
