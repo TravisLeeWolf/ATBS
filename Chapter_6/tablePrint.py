6d5733e6ce654efd020c3e23192d8acd4c7bb018
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-09 09:35:49 +0900

    0935

---
+++ Chapter_6/tablePrint.py
@@ -0,0 +1,9 @@
+#! python3
+# tablePrint.py - Prints a list of lists of strings in a neat table
+
+# Table containing items
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+# Function to print lists in a table
