57ab25a717588fa68f7ecf7cb42a9c91629b14e7
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-10 15:49:33 +0900

    1549

--- Chapter_7/pipeQues.py
+++ Chapter_7/pipeQues.py
@@ -9,4 +9,20 @@ mo2 = heroRegex.search('Tina Fey and Batman.')
 
 print(mo1.group())
 print(mo2.group())
+print()
 
+# Matching using a prefix
+batRegex = re.compile(r'Bat(man|mobile|copter)')
+mo = batRegex.search('The Batmobile lost a wheel.')
+
+print(mo.group())
+print('Bat is the prefix, the later half is ' + mo.group(1))
+print()
+
+# Optional matching with the question mark
+batGenderRegex = re.compile(r'Bat(wo)?man')
+mo3 = batGenderRegex.search('The Adventures of Batman.')
+mo4 = batGenderRegex.search('The Adventures of Batwoman.')
+
+print(mo3.group())
+print(mo4.group())
