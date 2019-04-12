8c92cf68c42ec84da646ffbce24cdb25c021f9df
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-12 23:46:40 +0900

    2346

--- Chapter_7/strongPass.py
+++ Chapter_7/strongPass.py
@@ -6,3 +6,12 @@
 # At least one digit
 
 # Use multiple regex to validate strenght
+
+# These functions can run a search and check if .group() is True/False
+# TODO: Create regex to check length of imput
+
+# TODO: Create regex to check uppercase and lowercase
+
+# TODO: Create regex to check for at least one digit
+
+# TODO: Loop to run trough each regex, if all True then accept password
