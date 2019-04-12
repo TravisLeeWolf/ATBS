43091486794fdf432d58db02433384c15c348dd5
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-12 23:37:09 +0900

    2237

---
+++ Chapter_7/strongPass.py
@@ -0,0 +1,8 @@
+#! python3
+# strongPass.py - Regex check that a password is strong
+
+# Eight characters long
+# Both uppercase and lowercase
+# At least one digit
+
+# Use multiple regex to validate strenght
