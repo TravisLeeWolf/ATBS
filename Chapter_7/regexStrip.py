ad7e072cd6d87dea530845ec5ceb250d70c0eb3f
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-12 23:41:22 +0900

    2341

---
+++ Chapter_7/regexStrip.py
@@ -0,0 +1,5 @@
+#! python3
+# regexStrip.py - A program that works the same as the .strip() function
+
+# If no other arguments are passed then remove whitespaces
+# Otherwise the second argument will strip whatever the user inputs
