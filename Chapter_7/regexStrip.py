c19418393875a2c287d5cee4065e618ea921e349
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-12 23:49:35 +0900

    2349

--- Chapter_7/regexStrip.py
+++ Chapter_7/regexStrip.py
@@ -3,3 +3,9 @@
 
 # If no other arguments are passed then remove whitespaces
 # Otherwise the second argument will strip whatever the user inputs
+
+# TODO: Create regex to remove whitespaces
+
+# TODO: Create function to accept user input and change regex format
+
+# TODO: Print out resulting string
