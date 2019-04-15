c1d0de87b84ec9c0c6ec1014e3ccd44afd5e6e46
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-15 10:40:22 +0900

    1040

---
+++ Chapter_8/openingFiles.py
@@ -0,0 +1,7 @@
+#! python3
+# openingFiles.py - Learning how to open files in Python
+
+#1. Call the open() function to return a File object
+#2. Call the read() or wirte() method on the File object
+#3. Close the file by calling the close() methond on the File object
+
