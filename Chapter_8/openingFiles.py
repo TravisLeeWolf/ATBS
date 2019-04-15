cdbb126dd87c59313c6f3937614fa4305ce750da
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-15 10:43:47 +0900

    1043

--- Chapter_8/openingFiles.py
+++ Chapter_8/openingFiles.py
@@ -5,3 +5,9 @@
 #2. Call the read() or wirte() method on the File object
 #3. Close the file by calling the close() methond on the File object
 
+# With open() you can only read data
+helloFile = open('S:\\Documents\\GitHub\\ATBS\\Chapter_8\\hello.txt')
+
+# Reading the file
+helloContent = helloFile.read()
+print(helloContent)
