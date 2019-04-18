7c9759f35143fd2c6baaf78ad01f3c717f973584
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-18 14:17:10 +0900

    1417

--- Chapter_7/regexStrip.py
+++ Chapter_7/regexStrip.py
@@ -4,7 +4,15 @@
 # If no other arguments are passed then remove whitespaces
 # Otherwise the second argument will strip whatever the user inputs
 
+# Import libraries
+import re
+
+randomText = 'Two plus two equals four.'
+
 # TODO: Create regex to remove whitespaces
+regexSpace = re.compile(r'\s', re.DOTALL)
+subText = '_'
+print(regexSpace.sub(subText, randomText))
 
 # TODO: Create function to accept user input and change regex format
 
