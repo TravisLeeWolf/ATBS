1498b8c22683d56cb995005768f24f36fcffeaa8
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-22 14:32:47 +0900

    1432

---
+++ Chapter_10/buggyAdd.py
@@ -0,0 +1,12 @@
+#! python3
+# buggyAdd.py - Learning how the Python debugger works
+
+print('Enter the first number to add:')
+first = input()
+print('Enter the second number to add:')
+second = input()
+print('Enter the third number to add:')
+third = input()
+print('The sum is ' + first + second + third)
+
+# Inputs need to be evaluated as int first and then printed as str
