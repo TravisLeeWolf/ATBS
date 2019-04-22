9bc0b07014335132b68361571df7fc56373b4281
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-22 13:43:04 +0900

    1343

---
+++ Chapter_10/trafficLight.py
@@ -0,0 +1,19 @@
+#! python3
+# trafficLight.py - Learning to use the assert function
+
+market_2nd = {'ns': 'green', 'ew': 'red'}
+mission_16th = {'ns': 'ref', 'ew': 'green'}
+
+def switchLights(stoplight):
+    for key in stoplight.keys():
+        if stoplight[key] == 'green':
+            stoplight[key] = 'yellow'
+        elif stoplight[key] == 'yellow':
+            stoplight[key] = 'red'
+        elif stoplight[key] == 'red':
+            stoplight[key] = 'green'
+    
+    # Adding assertiong to make sure at least one light is red
+    assert 'red' in stoplight.values(), 'Neither light is red! ' + str(stoplight)
+
+switchLights(market_2nd)
