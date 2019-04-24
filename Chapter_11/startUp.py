a751aee776b9d66253554d33cb440822eb53a558
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2019-04-24 16:10:48 +0900

    1610

---
+++ Chapter_11/startUp.py
@@ -0,0 +1,11 @@
+#! python3
+# startUp.py - Opens up frequently used programs
+
+# Import libraries
+import webbrowser
+
+# TODO: Ask user which sites to open
+
+# TODO: Loop to open all requested sites
+
+# Find out how to open multiple pages on one browser
