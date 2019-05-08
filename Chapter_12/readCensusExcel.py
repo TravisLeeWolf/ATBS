c835e5f3ca7a3be42b18f4eb45b49e4493938d33
Lee Wolf <AdminLeeWolf@users.noreply.github.com>
2019-05-08 22:33:40 +0900

    2233

--- Chapter_12/readCensusExcel.py
+++ Chapter_12/readCensusExcel.py
@@ -9,7 +9,7 @@ wb = openpyxl.load_workbook('censuspopdata.xlsx')
 sheet = wb.get_sheet_by_name('Population by Census Tract')
 countyData = {}
 
-# TODO: Fill in countyData with each county's population and tracts
+# Fill in countyData with each county's population and tracts
 print('Reading rows...')
 
 for row in range(2, sheet.max_row + 1):
@@ -18,5 +18,19 @@ for row in range(2, sheet.max_row + 1):
     county = sheet['C' + str(row)].value
     pop = sheet['D' + str(row)].value
 
-# TODO: Open a new text file and write the contents of countyData
+    # Make sure the key for this state exists
+    countyData.setdefault(state, {})
+    # Make sure the key for this county in this state exists
+    countyData[state].setdefault(county, {'tracts': 0, 'pop':0})
 
+    # Each row represents one census tract, so increment by one
+    countyData[state][county]['tracts'] += 1
+    # Increase the county pop by the pop in this census tract
+    countyData[state][county]['pop'] += int(pop)
+
+# Open a new text file and write the contents of countyData
+print('Writing results...')
+resultFile = open('census2010.py', 'w')
+resultFile.write('allData = ' + pprint.pformat(countyData))
+resultFile.close()
+print('Done.')
